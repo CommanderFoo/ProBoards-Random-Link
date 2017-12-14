71f03b67abad909adb30e6c558e76fffbae703a3
Peter <github@pixeldepth.net>
2017-12-14 22:58:49 +0000

    Request done

---
+++ README.md
@@ -0,0 +1,10 @@
+Random Link
+===========
+
+Add a button in your layout template.
+
+Example:
+
+`<button id="random-link-button">Click For Random Link</button>`
+
+http://support.proboards.com/thread/643616/link-randomizer
---
+++ src/random_link.js
@@ -0,0 +1,36 @@
+class Random_Link {
+
+	static init(){
+		this.PLUGIN_ID = "pd_random_link";
+		this.random_links = [];
+
+		this.setup();
+
+		if(this.random_links.length > 0){
+			$(this.ready.bind(this));
+		}
+	}
+
+	static ready(){
+		let $button = $("#random-link-button");
+
+		if($button.length == 1){
+			$button.on("click", () => {
+
+				let link = this.random_links[~~ (Math.random() * this.random_links.length)];
+
+				window.open(link.link);
+
+			});
+		}
+	}
+
+	static setup(){
+		let plugin = pb.plugin.get(this.PLUGIN_ID);
+
+		if(plugin && plugin.settings){
+			this.random_links = plugin.settings.links;
+		}
+	}
+
+}
